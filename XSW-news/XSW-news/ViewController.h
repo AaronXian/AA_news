5c4eba9baf62a2cad095c56fb3bbb5fda1fa19ed
AaronXian <aaronsinnn@outlook.com>
2016-04-23 09:44:26 +0800

    test

---
+++ XSW-news/XSW-news/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XSW-news
+//
+//  Created by Aaron sin on 16/4/23.
+//  Copyright © 2016年 Aaron sin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XSW-news/XSW-news/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XSW-news
+//
+//  Created by Aaron sin on 16/4/23.
+//  Copyright © 2016年 Aaron sin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"start");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
